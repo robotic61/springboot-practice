922be65846e61fb656f442639294d213785fc2dc
Ratchapong Tiebsornchai <goddogyohan@Ratchapongs-MacBook-Pro.local>
2026-04-27 10:00:21 +0700

    Initial commit

---
+++ src/main/java/com/forthems/ftnlabel/FtnlabelApplication.java
@@ -0,0 +1,13 @@
+package com.forthems.ftnlabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtnlabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtnlabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forthems/ftnlabel/FtnlabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forthems.ftnlabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtnlabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
